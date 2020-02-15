347aaca7b11c1a51492e3c0963686f4de5d566c3
Mark Hobson <markhobson@gmail.com>
2020-02-15 12:53:57 +0000

    Remove demo container on exit

--- demo/run.sh
+++ demo/run.sh
@@ -3,4 +3,4 @@
 DOCKER_IMAGE=markhobson/maven-chrome
 
 docker pull $DOCKER_IMAGE
-docker run -it -v "$PWD":/usr/src -w /usr/src $DOCKER_IMAGE mvn clean verify
+docker run --rm -it -v "$PWD":/usr/src -w /usr/src $DOCKER_IMAGE mvn clean verify
