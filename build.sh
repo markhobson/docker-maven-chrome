77052e7b76e9ef773d3710f13237d9185de49fd7
Mark Hobson <markhobson@gmail.com>
2020-12-16 23:35:35 +0000

    GH-11: Tag jdk-8 when building locally

--- build.sh
+++ build.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-docker build --pull -t maven-chrome jdk-8 \
+docker build --pull -t maven-chrome:jdk-8 -t maven-chrome jdk-8 \
 	&& docker build --pull -t maven-chrome:jdk-11 jdk-11 \
 	&& docker build --pull -t maven-chrome:jdk-15 jdk-15
