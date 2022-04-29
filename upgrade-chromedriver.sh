1f51b32c311d8b31c4a6cf040a6c4f3f6f3b5173
Mark Hobson <markhobson@gmail.com>
2022-04-29 09:52:32 +0100

    Add script to upgrade ChromeDriver to latest version

---
+++ upgrade-chromedriver.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+VERSION=$(curl -s https://chromedriver.storage.googleapis.com/LATEST_RELEASE)
+
+./set-arg.sh CHROME_DRIVER_VERSION $VERSION
+sed -i -E "s/^(\* ChromeDriver) [[:digit:]]+$/\1 $VERSION/g" README.md
+
+git commit -a -m "Upgrade ChromeDriver to $VERSION"
