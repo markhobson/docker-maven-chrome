f4a48d64afba28a8a37010c29c775acd43c1163e
Mark Hobson <markhobson@gmail.com>
2022-06-17 12:36:56 +0100

    Correct version pattern when upgrading ChromeDriver

--- upgrade-chromedriver.sh
+++ upgrade-chromedriver.sh
@@ -3,6 +3,6 @@
 VERSION=$(curl -s https://chromedriver.storage.googleapis.com/LATEST_RELEASE)
 
 ./set-arg.sh CHROME_DRIVER_VERSION $VERSION
-sed -i -E "s/^(\* ChromeDriver) [[:digit:]]+$/\1 $VERSION/g" README.md
+sed -i -E "s/^(\* ChromeDriver) [[:digit:].]+$/\1 $VERSION/g" README.md
 
 git commit -a -m "Upgrade ChromeDriver to $VERSION"
