ddc715ebee9955251e7cf1253a301da6bd396142
Mark Hobson <markhobson@gmail.com>
2023-06-02 12:12:02 +0100

    Don't commit package upgrades when up-to-date

--- upgrade-chrome.sh
+++ upgrade-chrome.sh
@@ -14,4 +14,9 @@ VERSION=$(echo $PACKAGE_VERSION | cut -d '-' -f1)
 ./set-arg.sh CHROME_VERSION $PACKAGE_VERSION
 sed -i -E "s/^(\* Chrome) [[:digit:].]+$/\1 $VERSION/g" README.md
 
-git commit -a -m "Upgrade Chrome to $VERSION"
+if [ $(git status --porcelain --untracked-files=no | wc -l) -ne 0 ]
+then
+  git commit -a -m "Upgrade Chrome to $VERSION"
+else
+  echo "Already up-to-date"
+fi
